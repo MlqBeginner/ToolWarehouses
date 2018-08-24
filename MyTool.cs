e4964162b798ec1e9ffe30d7fafff61fc5f388c7
MlqBeginner <35244073+MlqBeginner@users.noreply.github.com>
2018-08-24 18:34:50 +0800

    这是一个DataTable转List集合的工具类
    这是一个DataTable转List集合的工具类

---
+++ MyTool.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeLayers.Common
+{
+   public class MyTool
+    {
+        /// <summary>
+        /// DataSetToList
+        /// DateTable转List
+        /// </summary>
+        /// <typeparam name="T">转换类型</typeparam>
+        /// <param name="dataSet">数据源</param>
+        /// <param name="tableIndex">需要转换表的索引</param>
+        /// <returns></returns>
+        public List<T> DataTableToList<T>(DataTable dt)
+        {
+            //确认参数有效
+            if (dt == null )
+                return null;
+            List<T> list = new List<T>();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                //创建泛型对象
+                T _t = Activator.CreateInstance<T>();
+                //获取对象所有属性
+                PropertyInfo[] propertyInfo = _t.GetType().GetProperties();
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    foreach (PropertyInfo info in propertyInfo)
+                    {
+                        //属性名称和列名相同时赋值
+                        if (dt.Columns[j].ColumnName.ToUpper().Equals(info.Name.ToUpper()))
+                        {
+                            if (dt.Rows[i][j] != DBNull.Value)
+                            {
+                                info.SetValue(_t, dt.Rows[i][j], null);
+                            }
+                            else
+                            {
+                                info.SetValue(_t, null, null);
+                            }
+                            break;
+                        }
+                    }
+                }
+                list.Add(_t);
+            }
+            return list;
+        }
+    }
+}
